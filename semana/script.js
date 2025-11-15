f7dce4b08d002e47129c4a7e86cc42900d8adb8c
𝐌𝐞𝐥𝐢𝐬𝐬𝐚 𝐌𝐚𝐫𝐭𝐢𝐧𝐬 <martinsdossantosmelissa093@gmail.com>
2025-11-15 13:35:05 -0300

    Add files via upload

---
+++ semana/script.js
@@ -0,0 +1,18 @@
+function nomeDoDia() {
+      const numero = parseInt(document.getElementById('dia').value);
+      const res = document.getElementById('resultado');
+      let dia;
+
+      switch (numero) {
+        case 1: dia = 'Domingo'; break;
+        case 2: dia = 'Segunda-feira'; break;
+        case 3: dia = 'Terça-feira'; break;
+        case 4: dia = 'Quarta-feira'; break;
+        case 5: dia = 'Quinta-feira'; break;
+        case 6: dia = 'Sexta-feira'; break;
+        case 7: dia = 'Sábado'; break;
+        default: dia = 'Número inválido. Digite de 1 a 7.';
+      }
+
+      res.textContent = dia;
+    }
